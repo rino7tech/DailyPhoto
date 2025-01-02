9f81b18cb9a9e77a7fb4bb393e9993997465990f
rino7tech <rino7.tech@gmail.com>
2025-01-02 17:49:56 +0900

    first-commit

---
+++ DailyPhoto/DailyPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyPhotoApp.swift
+//  DailyPhoto
+//
+//  Created by 伊藤璃乃 on 2025/01/02.
+//
+
+import SwiftUI
+
+@main
+struct DailyPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
